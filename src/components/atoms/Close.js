92087ff13f2a3de3d215cee150db5d338d3cb0d0
matt.giertl <matus1giertl@gmail.com>
2019-11-20 17:37:31 +0100

    added custom components

---
+++ src/components/atoms/Close.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Close = styled.p({
+  margin: 0,
+  padding: 0,
+  color: 'red',
+  position: 'absolute',
+  right: '20px',
+  cursor: 'pointer',
+  top: '20px',
+});
+
+export default Close;
---
+++ src/components/atoms/Description.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Description = styled.p({
+  margin: 0,
+  padding: 0,
+  color: 'gray',
+});
+
+export default Description;
