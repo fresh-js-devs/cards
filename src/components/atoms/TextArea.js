7a86211a89f3c09dd05a9eb692d02cfb655f524a
matt.giertl <matus1giertl@gmail.com>
2019-11-20 20:49:53 +0100

    created separate components

---
+++ src/components/atoms/TextArea.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const TextArea = styled.textarea({
+  width: '300px',
+  minHeight: '100px',
+  marginBottom: '20px',
+});
+
+export default TextArea;
---
+++ src/components/organisms/Cards.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Cards = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '50px',
+  width: '70%',
+});
+
+export default Cards;
