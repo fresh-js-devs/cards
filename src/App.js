8bb3fe2093c84380e657de9742e541ffd4e6517f
matt.giertl <matus1giertl@gmail.com>
2019-11-20 17:37:45 +0100

    initial app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Card from './components/molecules/Card';
 
-function App() {
+import Users from './mocks/cards.json';
+
+const LayoutWrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '80%',
+  margin: '0 auto',
+});
+
+const Cards = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '50px',
+  width: '70%',
+});
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(Users);
+  }, []);
+
+  const handleCloseClicked = id => {
+    const filteredUsers = users.filter(u => u.id !== id);
+    setUsers(filteredUsers);
+  };
+
+  const renderUserCards = () =>
+    users.map(user => {
+      const { name, description, id } = user;
+      return (
+        <Card
+          name={name}
+          description={description}
+          key={id}
+          onCloseClick={() => handleCloseClicked(id)}
+        />
+      );
+    });
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LayoutWrapper>
+      <h1>Cards</h1>
+      <Cards>{renderUserCards()}</Cards>
+    </LayoutWrapper>
   );
-}
+};
 
 export default App;
