0d238e8e325d45f1c0b7731179fbcd03b739b7aa
matt.giertl <matus1giertl@gmail.com>
2019-11-20 20:49:42 +0100

    implemented add and delete functionalities, reformatted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
+
 import styled from '@emotion/styled';
+
 import Card from './components/molecules/Card';
+import Input from './components/atoms/Input';
+import TextArea from './components/atoms/TextArea';
+import Button from './components/atoms/Button';
+
+import Cards from './components/organisms/Cards';
+import Form from './components/organisms/Form';
 
 import Users from './mocks/cards.json';
 
@@ -12,16 +20,12 @@ const LayoutWrapper = styled.div({
   margin: '0 auto',
 });
 
-const Cards = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginTop: '50px',
-  width: '70%',
-});
-
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const inputsAreEmpty = name === '' || description === '';
 
   useEffect(() => {
     setUsers(Users);
@@ -30,6 +34,15 @@ const App = () => {
   const handleCloseClicked = id => {
     const filteredUsers = users.filter(u => u.id !== id);
     setUsers(filteredUsers);
+  };
+
+  const handleAddUserClicked = () => {
+    const newUser = {
+      id: users.length + 1,
+      name,
+      description,
+    };
+    setUsers([...users, newUser]);
   };
 
   const renderUserCards = () =>
@@ -44,10 +57,27 @@ const App = () => {
         />
       );
     });
-    
+
   return (
     <LayoutWrapper>
-      <h1>Cards</h1>
+      <h1>User Cards</h1>
+      <Form>
+        <Input
+          value={name}
+          placeholder="name"
+          onChange={e => setName(e.target.value)}
+          type="text"
+        />
+        <TextArea
+          placeholder="description"
+          onChange={e => setDescription(e.target.value)}
+          style={{ width: '300px', minHeight: '100px', marginBottom: '20px' }}
+          value={description}
+        />
+        <Button disabled={inputsAreEmpty} onClick={handleAddUserClicked}>
+          Add user
+        </Button>
+      </Form>
       <Cards>{renderUserCards()}</Cards>
     </LayoutWrapper>
   );
